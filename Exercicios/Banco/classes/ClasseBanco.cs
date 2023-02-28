68788e28343671606b1b99cbfa9fd50619e2cd20
Guilherme Leme de Matos <100931590+guilhermelemematos@users.noreply.github.com>
2023-02-28 12:37:32 -0300

    Add files via upload

---
+++ Exercicios/Banco/classes/ClasseBanco.cs
@@ -0,0 +1,31 @@
+namespace classes;
+
+class ClasseBanco{
+    public int agencia;
+    public string tipo = "";
+    public int valor;
+    public int conta;
+
+    public void depositar(){
+        valor++;
+    }
+
+     public void sacar(){
+        valor--;
+    }
+
+     public void transferir(){
+        valor++;
+    }
+
+     public void consulatr(){
+        conta--;
+    }
+
+    public void exibir(){
+        Console.WriteLine("Dados da conta:");
+        Console.WriteLine("Modelo:" + tipo);
+        Console.WriteLine("numero da conta" + conta);
+        Console.WriteLine("dinheiro na conta atual:" + valor);
+    }
+}
---
+++ Exercicios/Banco/classes/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Banco;
+class ClasseBanco
+{
+    static void Main(string[] args)
+    {
+        ClasseBanco banco1 = new ClasseBanco();
+    }
+
+   banco1.tipo = "corrente";
+    }
