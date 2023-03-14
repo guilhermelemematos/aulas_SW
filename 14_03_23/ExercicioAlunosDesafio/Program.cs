f2bb930425f4ac7fd7743a4b1043401e7d67a4b5
Guilherme Leme de Matos <100931590+guilhermelemematos@users.noreply.github.com>
2023-03-14 12:17:09 -0300

    Add files via upload

---
+++ 14_03_23/ExercicioAlunosDesafio/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ExercicioAlunos;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Alunos aluno1 = new Alunos();
+        Console.WriteLine("Digite seu nome: ");
+        aluno1.nome = Console.ReadLine();
+        
+        Console.WriteLine("Digite a nota1: ");
+        aluno1.nota1 = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Digite a nota2: ");
+        aluno1.nota2 = double.Parse(Console.ReadLine());
+
+        aluno1.mensagem();
+    }
+}
