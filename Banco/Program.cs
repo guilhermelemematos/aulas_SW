cd7f8340f10db6dd5c5d468154e170f08fcd438d
Guilherme Leme de Matos <100931590+guilhermelemematos@users.noreply.github.com>
2023-03-21 12:28:57 -0300

    21/03/2023

---
+++ Banco/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Banco;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Banco etec MCM");
+        Conta c = new Conta();
+        //c.Saldo = 1500;
+        c.Depositar(1500);
+        c.AjustaLimite(500);
+        c.Sacar(5000);
+
+        //Console.WriteLine("Seu saldo é de : " + c.Saldo + " e seu limite é de: " + c.Limite);
+        Console.WriteLine("Seu saldo é de: " + c.MostrarSaldo());
+    }
+}
