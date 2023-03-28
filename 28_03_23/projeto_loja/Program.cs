41ce4160574d088ad6333b3f235733895f5f4b8a
Guilherme Leme de Matos <100931590+guilhermelemematos@users.noreply.github.com>
2023-03-28 12:32:47 -0300

    Add files via upload

---
+++ 28_03_23/projeto_loja/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace projeto_loja;
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Produto p1 = new Produto();
+        //Produto p1 = new Produto("xyz");
+        Produto p1 = new Produto("xyz",10);
+        p1.MostraDados();
+    }
+}
---
+++ 28_03_23/projeto_pessoa/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace projeto_pessoa;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Pessoa p1 = new Pessoa("Guilherme", 16);
+        //p1.AlteraNome("Guilherme");
+        //p1.AlteraSobreNome("Leme");
+        //p1.AlteraIdade(16);
+        p1.MostraDados();
+    }
+}
