51dc3a3aac17de10b0431123b1789673332832d8
Guilherme Leme de Matos <100931590+guilhermelemematos@users.noreply.github.com>
2023-04-11 11:36:35 -0300

    Add files via upload

---
+++ agregação/Cartao.cs
@@ -0,0 +1,7 @@
+namespace agregação;
+class Cartao
+{
+    public string? Numero {get; set;}
+    public string? DataValidade {get; set;}
+    public Cliente? Cliente {get; set;} 
+}
---
+++ agregação/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace agregação;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(" Trabalhando com agregação!");
+
+        Cartao cdc = new Cartao();
+        Cliente cli = new Cliente();
+
+        cli.Nome = "Guilherme Leme";
+
+        cdc.Numero = "123456";
+        cdc.DataValidade = "04/28";
+
+        cdc.Cliente = cli;
+
+        Console.WriteLine("O nome do cliente é: " + cli.Nome);
+        Console.WriteLine("O numero do cartão é: " + cdc.Numero);
+        Console.WriteLine("A data de vadilidade é: " + cdc.DataValidade);
+        Console.WriteLine("O nome do cliente  AGREGADO é: " + cdc.Cliente.Nome);
+    }
+}
