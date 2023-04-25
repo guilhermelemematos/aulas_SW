47ed8f836fc172f81b9a6c1d8ef3ac4eb40c1c57
Guilherme Leme de Matos <100931590+guilhermelemematos@users.noreply.github.com>
2023-04-25 11:26:05 -0300

    25/04/23

---
+++ Bancov2/Conta.cs
@@ -0,0 +1,46 @@
+﻿namespace BancoV2;
+class Conta
+{
+    public int Numero {get; set;}
+    private double Saldo {get; set;}
+    public double Limite {get; set;}
+    public double ValorEmprestimo {get; set;}
+    public double Mes {get; set;}
+ 
+
+    public void Depositar(double valor){
+        this.Saldo += valor;
+    }
+
+    public void Sacar(double valor){
+        if(valor > this.Saldo + this.Limite){
+            Console.WriteLine("Você não pode realizar esse saque! Saldo insuficiente");
+        }else{
+        this.Saldo -= valor;
+        }
+    }
+
+    public double MostrarSaldo(){
+        return this.Saldo + this.Limite;
+    }
+
+    public void AjustaLimite(double valor){
+        this.Limite = valor;
+    }
+
+    public string Emprestimo(double emprestimo, int mes){
+        this.ValorEmprestimo = emprestimo;
+        this.Mes = mes;
+        double parcelaSemJuros = this.ValorEmprestimo / this.Mes;
+        double Juros = this.ValorEmprestimo * 0.06;
+        double ParcelaJuros = parcelaSemJuros + Juros;
+        double Total = ParcelaJuros * this.Mes;
+        string resultado = "O valor do seu empréstimo total é de: " + Total;
+
+        return resultado;
+    }
+
+    public void imprime(string texto){
+        Console.WriteLine(texto);
+    }
+}
---
+++ Bancov2/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace BancoV2;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Banco etec MCM");
+        Conta c = new Conta();
+        //c.Saldo = 1500;
+        c.Depositar(1500);
+        c.AjustaLimite(500);
+        c.Sacar(5000);
+
+        //Console.WriteLine("Seu saldo é de : " + c.Saldo + " e seu limite é de: " + c.Limite);
+        Console.WriteLine("Seu saldo é de: " + c.MostrarSaldo());
+        
+        c.imprime(c.Emprestimo(1000, 12));
+    }
+}
